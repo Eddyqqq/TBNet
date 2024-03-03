a80ec0bafefeffa840a73b3f57ba2607d2f78a3b
Eddyqqq <33630137+Eddyqqq@users.noreply.github.com>
2024-03-04 00:06:13 +0800

    Add files via upload

---
+++ slr_network.py
@@ -0,0 +1,138 @@
+import pdb
+import copy
+import utils
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from modules.criterions import IteLoss, ItaLoss
+from modules import BiLSTMLayer, TemporalConv
+from modules.clip import clip
+from modules.CrossAttn import CrossAttn
+
+def unpad_padded(x, xl, dim=0):
+    dims = list(range(len(x.shape)))
+    dims.insert(0, dims.pop(dim))
+    x = x.permute(*dims)
+    return [xi[:xli] for xi, xli in zip(x, xl)]
+
+class NormLinear(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(NormLinear, self).__init__()
+        self.weight = nn.Parameter(torch.Tensor(in_dim, out_dim))
+        nn.init.xavier_uniform_(self.weight, gain=nn.init.calculate_gain('relu'))
+
+    def forward(self, x):
+        outputs = torch.matmul(x, F.normalize(self.weight, dim=0))
+        return outputs
+
+
+class SLRModel(nn.Module):
+    def __init__(
+            self, num_classes, vit_type, conv_type, use_bn=False,
+            hidden_size=1024, gloss_dict=None, loss_weights=None,
+            weight_norm=True
+    ):
+        super(SLRModel, self).__init__()
+        self.decoder = None
+        self.loss = dict()
+        self.criterion_init()
+        self.num_classes = num_classes
+        self.loss_weights = loss_weights
+        self.clip, _ = clip.load(vit_type, device="cuda:0",
+                                 download_root="./modules/clip")
+        if self.training:
+            # IAM and IEM
+            self.crossattn = CrossAttn(hidden_size=1024)
+            self.glsfc = nn.Linear(512, hidden_size)
+        self.conv1d = TemporalConv(input_size=512,
+                                   hidden_size=hidden_size,
+                                   conv_type=conv_type,
+                                   use_bn=use_bn,
+                                   num_classes=num_classes)
+        self.decoder = utils.Decode(gloss_dict, num_classes, 'beam')
+        self.temporal_model = BiLSTMLayer(rnn_type='LSTM', input_size=hidden_size, hidden_size=hidden_size,
+                                          num_layers=2, bidirectional=True)
+        if weight_norm:
+            self.classifier = NormLinear(hidden_size, self.num_classes)
+        else:
+            self.classifier = nn.Linear(hidden_size, self.num_classes)
+        self.register_backward_hook(self.backward_hook)
+
+    def backward_hook(self, module, grad_input, grad_output):
+        for g in grad_input:
+            g[g != g] = 0
+
+    def masked_bn(self, inputs, len_x):
+        def pad(tensor, length):
+            return torch.cat([tensor, tensor.new(length - tensor.size(0), *tensor.size()[1:]).zero_()])
+
+        x = torch.cat([inputs[len_x[0] * idx:len_x[0] * idx + lgt] for idx, lgt in enumerate(len_x)])
+        x = self.clip.encode_image(x)
+        x = torch.cat([pad(x[sum(len_x[:idx]):sum(len_x[:idx + 1])], len_x[0])
+                       for idx, lgt in enumerate(len_x)])
+        return x
+
+    def forward(self, x, len_x, label=None, label_lgt=None, ann=None):
+        if len(x.shape) == 5:
+            # videos
+            batch, temp, channel, height, width = x.shape
+            inputs = x.reshape(batch * temp, channel, height, width)
+            framewise = self.masked_bn(inputs, len_x)
+            framewise = framewise.reshape(batch, temp, -1).transpose(1, 2)
+        else:
+            # frame-wise features
+            framewise = x
+
+        conv1d_outputs = self.conv1d(framewise, len_x)
+        # x: T, B, C
+        x = conv1d_outputs['visual_feat']
+        lgt = conv1d_outputs['feat_len']
+        tm_outputs = self.temporal_model(x, lgt)  # T, B, C
+        outputs = self.classifier(tm_outputs['predictions'])
+        (gls_emd, vis_emd) = self.calculate_similarity(tm_outputs['predictions'], lgt, ann) if self.training \
+            else (None, None)
+        pred = None if self.training \
+            else self.decoder.decode(outputs, lgt, batch_first=False, probs=False)
+        conv_pred = None if self.training \
+            else self.decoder.decode(conv1d_outputs['conv_logits'], lgt, batch_first=False, probs=False)
+        return {
+            "framewise_features": framewise,
+            "visual_features": x,
+            "feat_len": lgt,
+            "conv_logits": conv1d_outputs['conv_logits'],
+            "sequence_logits": outputs,
+            "conv_sents": conv_pred,
+            "recognized_sents": pred,
+            "vis_emd": vis_emd,
+            "gls_emd": gls_emd,
+        }
+
+    def criterion_calculation(self, ret_dict, label, label_lgt):
+        loss = 0
+        for k, weight in self.loss_weights.items():
+            if k == 'ConvCTC':
+                loss += weight * self.loss['CTCLoss'](ret_dict["conv_logits"].log_softmax(-1),
+                                                      label.cpu().int(), ret_dict["feat_len"].cpu().int(),
+                                                      label_lgt.cpu().int()).mean()
+            elif k == 'SeqCTC':
+                loss += weight * self.loss['CTCLoss'](ret_dict["sequence_logits"].log_softmax(-1),
+                                                      label.cpu().int(), ret_dict["feat_len"].cpu().int(),
+                                                      label_lgt.cpu().int()).mean()
+            elif k == 'Dist':
+                loss += weight * self.loss['distillation'](ret_dict["conv_logits"],
+                                                           ret_dict["sequence_logits"].detach(),
+                                                           use_blank=False)
+            elif k == 'IteLoss':
+                IteLoss = weight * self.loss['IteLoss'](ret_dict["gls_emd"], ret_dict["vis_emd"], label.cpu(), label_lgt)
+                loss += IteLoss
+            elif k == 'ItaLoss':
+                ItaLoss = weight * self.loss['ItaLoss'](ret_dict["gls_emd"], ret_dict["vis_emd"], label.cpu(), label_lgt)
+                loss += ItaLoss
+        return {"total_loss": loss, "IteLoss": IteLoss, "ItaLoss": ItaLoss}
+
+    def criterion_init(self):
+        self.loss['CTCLoss'] = torch.nn.CTCLoss(reduction='none', zero_infinity=False)
+        self.loss['IteLoss'] = IteLoss()
+        self.loss['ItaLoss'] = ItaLoss()
+        return self.loss
